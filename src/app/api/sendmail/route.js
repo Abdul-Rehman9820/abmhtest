f1f5b983250f533594e072ee4a2bfbc5af1963e1
Abdul Rehman Ansari <abdullahansari982076@gmail.com>
2023-11-10 19:22:08 +0530

    con api

---
+++ src/app/api/sendmail/route.js
@@ -0,0 +1,64 @@
+	
+
+import { NextResponse } from 'next/server'
+
+import { Resend } from 'resend';
+
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+export async function POST(req, content) {
+
+
+        try {
+
+
+          const body = await req.json(); // Get the search value from the URL
+
+          const username = body.username;
+          const userPhone = body.userPhone;
+          const usermail = body.usermail;
+          const userlocation = body.userlocation;
+          const usermessage = body.usermessage;
+
+
+           var maildatahtlm = `<div>
+
+                                <p>Name = ${username}</p>
+                                <p>Phone  = ${userPhone}</p>
+                                <p>Email = ${usermail}</p>
+                                <p>Location = ${userlocation}</p>
+                                <p>Message  = ${usermessage}</p>
+          
+                           <div>`;
+
+
+
+          const data = await resend.emails.send({
+            from: 'ABMH Enquiry Form <abdul.a@mindframeindia.com>',
+            to: ['abdul.a@mindframeindia.com','prathamesh.k@mindframeindia.com' , 'abdullahansari982076@gmail.com'],
+            subject: 'New Enquiry',
+            // react: 'John' ,
+            // text: 'This is text a test email.',
+            // html: '<p>This is a test email html.</p>',
+            html: maildatahtlm,
+          });
+          
+
+          return NextResponse.json({ status: 200, message: 'mail send', data: data });
+
+
+
+        } catch (e) {
+
+
+          return NextResponse.json({ status: 500, message: 'Not working API', error : e });
+
+
+        }
+
+
+
+}
+
+
--- src/app/contact-us/page.js
+++ src/app/contact-us/page.js
@@ -1,10 +1,122 @@
 
 
+"use client";
+
+import {  useState } from "react";
+
 
 import './contact-us.css'
 
 
 const Contactus = () => {
+
+
+
+  const [uname, setUname] = useState("");
+  const [uphone, setPhone] = useState("");
+  const [uemail, setUemail] = useState("");
+  const [ulocation, setLocation] = useState("");
+  const [umessage, setMessage] = useState("");
+
+
+
+
+  const handleChange = (event) => {
+
+      const { name, value } = event.target;
+      if (name === "username") {
+          setUname(value);
+      } else if (name === "userPhone") {
+          setPhone(value);
+      }else if (name === "usermail") {
+          setUemail(value);
+      }else if (name === "userlocation") {
+          setLocation(value);
+      }else if (name === "usermessage") {
+          setMessage(value);
+      }
+
+  };
+
+  const handleSubmit = async (event) => {
+      event.preventDefault();
+
+
+     var messagebox = document.getElementById('apirespose');
+     var submitbutton = document.getElementById('submitbtb');
+
+
+     submitbutton.disabled = true ; // hide button to duble click
+     messagebox.innerHTML = '';  // do empty for resubmit
+
+      console.log(uname);
+      console.log(uphone);
+      console.log(uemail);
+      console.log(ulocation);
+      console.log(umessage);
+
+
+   // api level code 
+      const response = await fetch(`${process.env.NEXT_PUBLIC_Web_Domin}/api/sendmail`, {
+          method: "POST",
+          body: JSON.stringify({
+              username: uname,
+              userPhone: uphone,
+              usermail: uemail,
+              userlocation: ulocation,
+              usermessage: umessage,
+          }),
+        });
+
+
+        if (!response.ok) {
+
+          // throw new Error("Network response was not ok");
+
+          messagebox.innerHTML = 'Something went wrong';
+
+          submitbutton.disabled = false ;
+
+        }else{
+
+                  const result = await response.json();
+      
+                  console.log(result);
+      
+                  if(result.message == "mail send"){
+
+
+                      setUname("");
+                      setPhone("");
+                      setUemail("");
+                      setLocation("");
+                      setMessage("");  
+                      
+                      
+      
+                  submitbutton.disabled = false ;
+
+                  messagebox.innerHTML = 'Mail send successfully';
+      
+                  }else{
+      
+                  submitbutton.disabled = false ;
+
+                  messagebox.innerHTML = 'Something went wrong';
+      
+                  }
+      
+
+        }
+     // api level code 
+
+
+  
+  };
+
+
+
+
 
 
   return (
@@ -49,32 +161,59 @@ const Contactus = () => {
                               Feel Free to contact us any time. We will get back
                               to you as soon as we can!.
                             </p>
-                            <input
-                              type="text"
-                              className="form-control form-group"
-                              placeholder="Name"
-                            />
-                            <input
-                              type="text"
-                              className="form-control form-group"
-                              placeholder="Name"
-                            />
-                            <input
-                              type="text"
-                              className="form-control form-group"
-                              placeholder="Name"
-                            />
-                            <input
-                              type="text"
-                              className="form-control form-group"
-                              placeholder="Email"
-                            />
-                            <textarea
-                              className="form-control form-group"
-                              placeholder="Message"
-                              defaultValue={""}
-                            />
-                            <button className="contact_form_submit">Send</button>
+                            
+
+                            <form onSubmit={handleSubmit}>
+                                  <input
+                                      type="text"
+                                      className="form-control form-group"
+                                      name="username"
+                                      placeholder="Your Name"
+                                      value={uname}
+                                      onChange={handleChange}
+                                      required
+                                  />
+                                  <input
+                                      type="text"
+                                      className="form-control form-group"
+                                      name="userPhone"
+                                      placeholder="Your Phone"
+                                      value={uphone}
+                                      onChange={handleChange}
+                                      required
+                                  />
+                                  <input
+                                      type="email"
+                                      className="form-control form-group"
+                                      name="usermail"
+                                      placeholder="Your Email"
+                                      value={uemail}
+                                      onChange={handleChange}
+                                      required
+                                  />
+                                  <input
+                                      type="text"
+                                      className="form-control form-group"
+                                      name="userlocation"
+                                      placeholder="Your Location"
+                                      value={ulocation}
+                                      onChange={handleChange}
+                                      required
+                                  />
+                                  <textarea
+                                      name="usermessage"
+                                      className="form-control form-group"
+                                      placeholder="Your Message"
+                                      value={umessage}
+                                      onChange={handleChange}
+                                      required
+                                  />
+                                  <button type="submit" id="submitbtb" className="contact_form_submit">Submit</button>
+                            </form>
+
+                            <div id="apirespose"></div>
+
+                            
                           </div>
                         </div>
                       </div>
