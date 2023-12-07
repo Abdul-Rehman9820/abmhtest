cc90e9650f1f0cf2ae0064faee01dbbb7a0e5cb5
Abdul Rehman Ansari <abdullahansari982076@gmail.com>
2023-12-07 17:27:56 +0530

    spe

---
+++ src/app/specialties/pediatric-intensivist/page.js
@@ -0,0 +1,246 @@
+
+"use client";
+
+import { useEffect, useState } from "react";
+import Image from 'next/image';
+import Link from 'next/link';
+
+import '../specialityviewpagecommon.css'
+
+
+
+const Andrologyandsexualmedicine = () => {
+
+
+  // store data here
+  const [data, setData] = useState(['']);
+
+  // for profile late loading
+  const [isLoading, setIsLoading] = useState(true); // Declare the isLoading variable
+
+  //---- for api data
+
+
+  useEffect(() => {
+
+
+    // api for profile
+
+
+    async function fetchData() {
+      try {
+        const response = await fetch(`${process.env.NEXT_PUBLIC_Web_Domin}/api/profilebyspecilitypage`, {
+          method: "POST",
+          body: JSON.stringify({
+            usersearch: "63",
+          }),
+        });
+
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+
+        const result = await response.json();
+
+        // Wait for API response to return before initializing the slider
+        // await new Promise((resolve) => setTimeout(resolve, 500));
+
+        setData(result.data);
+        setIsLoading(false);
+        console.log(result.data);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+
+    fetchData();
+
+
+    // api for profile
+
+
+
+  }, []);
+
+  // Group profiles into sets of three
+  const groupedProfiles = data.reduce((accumulator, profile, index) => {
+    if (index % 3 === 0) {
+      accumulator.push([profile]);
+    } else {
+      accumulator[accumulator.length - 1].push(profile);
+    }
+    return accumulator;
+  }, []);
+
+
+  return (
+
+
+    <>
+      <title>Pediatric Intensivist</title>
+
+      {/* common heading div start*/}
+      <div className="parentcontainerwhi">
+        <div className="customcontainer">
+          <div className="commonheaddiv">
+            <div className="commonheadcontainer">
+              <div className="breadcrumbs">
+                <div className="breadcrumbsdata">
+                  Home / Our Specialties / <span className="breadcrumbsactive">Pediatric Intensivist</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* common heading div end*/}
+      {/* common heading banner start */}
+      <div className="mainheadingbanner">
+        <div className="mainheadingbandiv">
+          <Image width={2000} height={400} src="/specialityviewimag/Multi-specialities-banner.jpg" alt="icon" />
+        </div>
+      </div>
+      {/* common heading banner end */}
+
+
+      {/* specialties  start*/}
+
+
+
+
+
+
+
+      {/* team of expert slider start*/}
+      <div className="parentcontainerwhi">
+        <div className="customcontainer">
+          <div className="specsingleDoclist">
+            <div className="Specialtiesheadgr">
+              <h2 className="spetileh1gr">Our Team Of Experts</h2>
+              <span className="headbordergr" />
+            </div>
+
+
+            <div className="ourteamparsingle">
+
+              <div
+                id="carouselExampleIndicators"
+                className="carousel slide carousel-fade"
+                data-bs-ride="carousel"
+              >
+
+                <div className="carousel-inner">
+
+
+                  {isLoading ? (
+                    <div className="loading-message">Loading doctors...</div>
+                  ) : data.length === 0 ? (
+                    <div className="empty-message">No doctors found</div>
+                  ) : (
+                    groupedProfiles.map((item, index) => (
+                      <div className={`carousel-item ${index === 0 ? 'active' : ''}`} key={index}>
+                        <div className="row mycentr">
+                          {item.map((profile, profileIndex) => (
+
+
+                            <div className="ourteamslidchilbosingle" key={profileIndex}>
+                              <div className="ourteamslidebox">
+                                <div className="ourteambox1single">
+                                  <div className="ourteamimg">
+                                    <Image width={400} height={400} src={`/DoctorsProfileimages/${profile.DoctorProfileImage}`} alt="img" />
+                                  </div>
+                                  <div className="ourteamnamesing">
+                                    <h2>{profile.DoctorFName} {profile.DoctorLName}</h2>
+                                  </div>
+                                  <div className="ourteamspec">
+                                    <h2>{profile.DoctorDesignation}</h2>
+                                  </div>
+                                  <div className="ourteamexpi">
+                                    <h2>{profile.DoctorExperience}</h2>
+                                  </div>
+                                </div>
+                                <div className="ourteambox2">
+                                  <div className="ourteambox2flex">
+
+                                    <Link className="bookappoteam" href={`/doctor-profile?doc=${profile.DoctorNameSlug}`}>
+                                      View Profile
+                                    </Link>
+
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
+
+
+                          ))}
+                        </div>
+                      </div>
+                    ))
+                  )}
+
+
+
+
+                </div>
+
+
+
+                {data.length > 3 && (
+                  <>
+                    <button
+                      className="carousel-control-prev"
+                      type="button"
+                      data-bs-target="#carouselExampleIndicators"
+                      data-bs-slide="prev"
+                    >
+                      <span className="carousel-control-prev-icon smallimg" aria-hidden="true">
+                        <Image width={400} height={400} src="/homeimg/nextleft.png" alt="img" />
+                      </span>
+                      <span className="visually-hidden">Previous</span>
+                    </button>
+
+                    <button
+                      className="carousel-control-next"
+                      type="button"
+                      data-bs-target="#carouselExampleIndicators"
+                      data-bs-slide="next"
+                    >
+                      <span className="carousel-control-next-icon smallimg" aria-hidden="true">
+                        <Image width={400} height={400} src="/homeimg/nextright.png" alt="img" />
+                      </span>
+                      <span className="visually-hidden">Next</span>
+                    </button>
+                  </>
+                )}
+
+
+
+              </div>
+
+
+
+
+
+            </div>
+
+          </div>
+        </div>
+      </div>
+      {/* team of expert slider end*/}
+
+
+      {/* specialties end*/}
+    </>
+
+
+
+
+  );
+
+
+};
+
+
+
+export default Andrologyandsexualmedicine;
