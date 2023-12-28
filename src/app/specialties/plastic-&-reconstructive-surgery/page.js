f2791f8de3544948772916d49c1562954285ee12
Abdul Rehman Ansari <abdullahansari982076@gmail.com>
2023-12-29 00:29:21 +0530

    dd

--- src/app/specialties/plastic-&-reconstructive-surgery/page.js
+++ src/app/specialties/plastic-&-reconstructive-surgery/page.js
@@ -104,6 +104,128 @@ const Accidentandemergency = () => {
       {/* common heading banner end */}
 
 
+      {/* team of expert slider start*/}
+      <div className="parentcontainerwhi">
+        <div className="customcontainer">
+          <div className="specsingleDoclist">
+            <div className="Specialtiesheadgr">
+              <h2 className="spetileh1gr">Our Team Of Experts</h2>
+              <span className="headbordergr" />
+            </div>
+
+
+            <div className="ourteamparsingle">
+
+              <div
+                id="carouselExampleIndicators"
+                className="carousel slide"
+                data-bs-ride="carousel"
+              >
+
+                <div className="carousel-inner">
+
+
+                  {isLoading ? (
+                    <div className="loading-message">Loading doctors...</div>
+                  ) : data.length === 0 ? (
+                    <div className="empty-message">No doctors found</div>
+                  ) : (
+                    groupedProfiles.map((item, index) => (
+                      <div className={`carousel-item ${index === 0 ? 'active' : ''}`} key={index}>
+                        <div className="row mycentr">
+                          {item.map((profile, profileIndex) => (
+
+
+                            <div className="ourteamslidchilbosingle" key={profileIndex}>
+
+                              <Link href={`/doctor-profile?doc=${profile.DoctorNameSlug}`}>
+                                <div className="ourteamslidebox">
+                                  <div className="ourteambox1single">
+                                    <div className="ourteamimg">
+                                      <Image width={400} height={400} src={`/DoctorsProfileimages/${profile.DoctorProfileImage}`} alt="img" />
+                                    </div>
+                                    <div className="ourteamnamesing">
+                                      <h2>Dr {profile.DoctorFName} {profile.DoctorLName}</h2>
+                                    </div>
+                                    <div className="ourteamspec">
+                                      <h2>{profile.DoctorDesignation}</h2>
+                                    </div>
+                                    <div className="ourteamexpi">
+                                      <h2>{profile.DoctorExperience}</h2>
+                                    </div>
+                                  </div>
+                                  <div className="ourteambox2">
+                                    <div className="ourteambox2flex">
+
+                                      <Link className="bookappoteam" href={`/doctor-profile?doc=${profile.DoctorNameSlug}`}>
+                                        View Profile
+                                      </Link>
+
+                                    </div>
+                                  </div>
+                                </div>
+                              </Link>
+
+                            </div>
+
+
+                          ))}
+                        </div>
+                      </div>
+                    ))
+                  )}
+
+
+
+
+                </div>
+
+
+
+                {data.length > 3 && (
+                  <>
+                    <button
+                      className="carousel-control-prev"
+                      type="button"
+                      data-bs-target="#carouselExampleIndicators"
+                      data-bs-slide="prev"
+                    >
+                      <span className="carousel-control-prev-icon smallimg" aria-hidden="true">
+                        <Image width={400} height={400} src="/homeimg/nextleft.png" alt="img" />
+                      </span>
+                      <span className="visually-hidden">Previous</span>
+                    </button>
+
+                    <button
+                      className="carousel-control-next"
+                      type="button"
+                      data-bs-target="#carouselExampleIndicators"
+                      data-bs-slide="next"
+                    >
+                      <span className="carousel-control-next-icon smallimg" aria-hidden="true">
+                        <Image width={400} height={400} src="/homeimg/nextright.png" alt="img" />
+                      </span>
+                      <span className="visually-hidden">Next</span>
+                    </button>
+                  </>
+                )}
+
+
+
+              </div>
+
+
+
+
+
+            </div>
+
+          </div>
+        </div>
+      </div>
+      {/* team of expert slider end*/}
+
+
       {/* specialties  start*/}
 
 
