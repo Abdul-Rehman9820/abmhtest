fee98a23aa514848d7d45c14a7b27c9203ad7e35
Abdul Rehman Ansari <abdullahansari982076@gmail.com>
2024-05-09 16:36:50 +0530

    updated bio medical page

--- src/app/biomedical-waste/page.js
+++ src/app/biomedical-waste/page.js
@@ -17,7 +17,7 @@ const WellnessPack = () => {
   useEffect(() => {
 
 
-   window.document.scrollingElement?.scrollTo(0, 0);  // for top scroll
+    window.document.scrollingElement?.scrollTo(0, 0);  // for top scroll
 
   }, []);
 
@@ -56,6 +56,10 @@ const WellnessPack = () => {
 
 
           <div className="biomedicalcontainer mt-5 mb-5">
+
+
+
+            {/*Main accordion start*/}
             <div className="accordion" id="accordionExample">
 
 
@@ -65,14 +69,157 @@ const WellnessPack = () => {
                     className="accordion-button collapsed"
                     type="button"
                     data-bs-toggle="collapse"
-                    data-bs-target="#collapse1"
+                    data-bs-target="#collapse2024"
                     aria-expanded="true"
                   >
-                    Biomedical Waste Report
+                    Biomedical Waste Report 2024
                   </button>
                 </h2>
                 <div
-                  id="collapse1"
+                  id="collapse2024"
+                  className="accordion-collapse collapse"
+                  data-bs-parent="#accordionExample"
+                >
+                  <div className="accordion-body">
+                    <div className="newfdata">
+
+
+                      {/* inner sub accour */}
+
+
+                      <div className="accordion" id="accordionSub2024">
+
+
+
+                        <div className="accordion-item">
+                          <h2 className="accordion-header">
+                            <button
+                              className="accordion-button collapsed"
+                              type="button"
+                              data-bs-toggle="collapse"
+                              data-bs-target="#collapse2024Mar"
+                              aria-expanded="true"
+                            >
+                              Biomedical Waste Report March 2024
+                            </button>
+                          </h2>
+                          <div
+                            id="collapse2024Mar"
+                            className="accordion-collapse collapse"
+                            data-bs-parent="#accordionSub2024"
+                          >
+                            <div className="accordion-body">
+                              <div className="newfdata">
+
+
+                                <div className="bioimg">
+
+                                  <Image width={2000} height={2000} src="/biomedical-waste-pdf-img/bio-m-2024-march1.jpg" alt="Biomedical Waste Report" />
+                                  <Image width={2000} height={2000} src="/biomedical-waste-pdf-img/bio-m-2024-march2.jpg" alt="Biomedical Waste Report" />
+
+                                </div>
+
+
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+
+                        <div className="accordion-item">
+                          <h2 className="accordion-header">
+                            <button
+                              className="accordion-button collapsed"
+                              type="button"
+                              data-bs-toggle="collapse"
+                              data-bs-target="#collapse2024Feb"
+                              aria-expanded="true"
+                            >
+                              Biomedical Waste Report February 2024
+                            </button>
+                          </h2>
+                          <div
+                            id="collapse2024Feb"
+                            className="accordion-collapse collapse"
+                            data-bs-parent="#accordionSub2024"
+                          >
+                            <div className="accordion-body">
+                              <div className="newfdata">
+
+                                <div className="bioimg">
+
+                                  <Image width={2000} height={2000} src="/biomedical-waste-pdf-img/bio-m-2024-feb1.jpg" alt="Biomedical Waste Report" />
+                                  <Image width={2000} height={2000} src="/biomedical-waste-pdf-img/bio-m-2024-feb2.jpg" alt="Biomedical Waste Report" />
+
+                                </div>
+
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+
+
+                        <div className="accordion-item">
+                          <h2 className="accordion-header">
+                            <button
+                              className="accordion-button collapsed"
+                              type="button"
+                              data-bs-toggle="collapse"
+                              data-bs-target="#collapse2024Jan"
+                              aria-expanded="true"
+                            >
+                              Biomedical Waste Report January 2024
+                            </button>
+                          </h2>
+                          <div
+                            id="collapse2024Jan"
+                            className="accordion-collapse collapse"
+                            data-bs-parent="#accordionSub2024"
+                          >
+                            <div className="accordion-body">
+                              <div className="newfdata">
+
+
+                                <div className="bioimg">
+
+                                  <Image width={2000} height={2000} src="/biomedical-waste-pdf-img/bio-m-2024-jan1.jpg" alt="Biomedical Waste Report" />
+                                  <Image width={2000} height={2000} src="/biomedical-waste-pdf-img/bio-m-2024-jan2.jpg" alt="Biomedical Waste Report" />
+
+                                </div>
+
+
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+
+
+
+                      </div>
+
+
+                      {/* inner sub accour */}
+
+
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+
+              <div className="accordion-item">
+                <h2 className="accordion-header">
+                  <button
+                    className="accordion-button collapsed"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#collapse2023"
+                    aria-expanded="true"
+                  >
+                    Biomedical Waste Report 2023
+                  </button>
+                </h2>
+                <div
+                  id="collapse2023"
                   className="accordion-collapse collapse"
                   data-bs-parent="#accordionExample"
                 >
@@ -92,6 +239,10 @@ const WellnessPack = () => {
 
 
             </div>
+            {/*Main accordion end*/}
+
+
+
           </div>
 
 
