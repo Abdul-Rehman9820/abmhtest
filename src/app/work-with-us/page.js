ee6c2becadfc656955679e8904c86f8c2d151ce7
Abdul Rehman Ansari <abdullahansari982076@gmail.com>
2024-06-22 17:49:13 +0530

    jd added

--- src/app/work-with-us/page.js
+++ src/app/work-with-us/page.js
@@ -56,211 +56,18 @@ const Workwithus = () => {
 
           {/* comment if no opening*/}
 
-          <div className="workwithuscont">
+          {/* <div className="workwithuscont">
 
             <h4>There are no current openings</h4>
 
-          </div>
+          </div> */}
 
           {/* comment if no opening*/}
 
-          {/* <div className="workwithuscont">
+          
 
 
-            <div className="accordion" id="accordionExample">
-
-              <div className="accordion-item">
-                <h2 className="accordion-header" id="headingOne">
-                  <button
-                    className="accordion-button"
-                    type="button"
-                    data-bs-toggle="collapse"
-                    data-bs-target="#collapseOne"
-                    aria-expanded="true"
-                    aria-controls="collapseOne"
-                  >
-                    Medical Staff
-                  </button>
-                </h2>
-                <div
-                  id="collapseOne"
-                  className="accordion-collapse collapse show"
-                  aria-labelledby="headingOne"
-                  data-bs-parent="#accordionExample"
-                >
-                  <div className="accordion-body">
-                    <div className="mytabdata">
-                      <table className="myperstable">
-                        <thead>
-                          <tr>
-                            <th>Position</th>
-                            <th>Experience Required</th>
-                            <th>Last Date</th>
-                            <th>Job Description</th>
-                            <th>Action</th>
-                          </tr>
-                        </thead>
-                        <tbody>
-                          <tr>
-                            <td>Medical Officer</td>
-                            <td>2+ Years</td>
-                            <td>20 November, 2023</td>
-                            <td>
-                              <div className="applybtn">
-                                <span className="downpdg">
-                                  <Link href="">
-                                    <Image width={100} height={100}
-                                      src="/commonimages/download-pdf.png"
-                                      className="donwpdfimg"
-                                      alt="pdg"
-                                    />
-                                  </Link>
-                                </span>
-                             
-                              </div>
-                            </td>
-                            <td>
-                              <div className="applybtn">
-                             
-                                <span>
-                                  <Link className="donwpdftext" href="/work-with-us-form">
-                                    APPLY
-                                  </Link>
-                                </span>
-                              </div>
-                            </td>
-                          </tr>
-                          <tr>
-                            <td>Medical Officer</td>
-                            <td>2+ Years</td>
-                            <td>20 November, 2023</td>
-                            <td>
-                              <div className="applybtn">
-                                <span className="downpdg">
-                                  <Link href="">
-                                    <Image width={100} height={100}
-                                      src="/commonimages/download-pdf.png"
-                                      className="donwpdfimg"
-                                      alt="pdg"
-                                    />
-                                  </Link>
-                                </span>
-                             
-                              </div>
-                            </td>
-                            <td>
-                              <div className="applybtn">
-                             
-                                <span>
-                                  <Link className="donwpdftext" href="/work-with-us-form">
-                                    APPLY
-                                  </Link>
-                                </span>
-                              </div>
-                            </td>
-                          </tr>
-                         
-
-                        </tbody>
-                      </table>
-                    </div>
-                  </div>
-                </div>
-              </div>
-
-              <div className="accordion-item">
-                <h2 className="accordion-header" id="headingThree">
-                  <button
-                    className="accordion-button collapsed"
-                    type="button"
-                    data-bs-toggle="collapse"
-                    data-bs-target="#collapseThree"
-                    aria-expanded="false"
-                    aria-controls="collapseThree"
-                  >
-                    Nursing
-                  </button>
-                </h2>
-                <div
-                  id="collapseThree"
-                  className="accordion-collapse collapse"
-                  aria-labelledby="headingThree"
-                  data-bs-parent="#accordionExample"
-                >
-                  <div className="accordion-body">
-                    <div className="mytabdata">
-                      <table className="myperstable">
-                        <thead>
-                          <tr>
-                            <th>Position</th>
-                            <th>Experience Required</th>
-                            <th>Last Date</th>
-                            <th>Action</th>
-                          </tr>
-                        </thead>
-                        <tbody>
-                          <tr>
-                            <td>Nursing</td>
-                            <td>2+ Years</td>
-                            <td>20 November, 2023</td>
-                            <td>
-                              <div className="applybtn">
-                                <span className="downpdg">
-                                  <Link href="">
-                                    <Image width={100} height={100}
-                                      src="/commonimages/download-pdf.png"
-                                      className="donwpdfimg"
-                                      alt="pdg"
-                                    />
-                                  </Link>
-                                </span>
-                                <span>
-                                  <Link className="donwpdftext" href="/work-with-us-form">
-                                    APPLY
-                                  </Link>
-                                </span>
-                              </div>
-                            </td>
-                          </tr>
-                          <tr>
-                            <td>Nursing</td>
-                            <td>2+ Years</td>
-                            <td>20 November, 2023</td>
-                            <td>
-                              <div className="applybtn">
-                                <span className="downpdg">
-                                  <Link href="">
-                                    <Image width={100} height={100}
-                                      src="/commonimages/download-pdf.png"
-                                      className="donwpdfimg"
-                                      alt="pdg"
-                                    />
-                                  </Link>
-                                </span>
-                                <span>
-                                  <Link className="donwpdftext" href="/work-with-us-form">
-                                    APPLY
-                                  </Link>
-                                </span>
-                              </div>
-                            </td>
-                          </tr>
-                        </tbody>
-                      </table>
-                    </div>
-                  </div>
-                </div>
-              </div>
-
-            
-            </div>
-
-
-          </div> */}
-
-
-
-          {/* <div className="workwithuscont">
+          <div className="workwithuscont">
 
 
             <div className="accordion" id="accordionExample">
@@ -289,8 +96,9 @@ const Workwithus = () => {
                       <table className="myperstable">
                         <thead>
                           <tr>
+                            <th>Department</th>
                             <th>Position</th>
-                            <th>Last Date To Apply</th>
+                            <th>Experience</th>
                             <th>Job Description</th>
                             <th>Action</th>
                           </tr>
@@ -299,12 +107,13 @@ const Workwithus = () => {
 
 
                           <tr>
-                            <td>AGM Finance Application</td>
-                            <td> --- </td>
+                            <td>IT & Networking</td>
+                            <td>IT Manager</td>
+                            <td>10-12 Years</td>
                             <td>
                               <div className="applybtn">
                                 <span className="downpdg">
-                                  <Link href="/JobPDF/JD-AGM Finance Application-17-2-24.pdf" target="_blank" >
+                                  <Link href="/JobPDF/JD-IT-22-6-24.pdf" target="_blank" >
                                     <Image width={100} height={100}
                                       src="/commonimages/download-pdf.png"
                                       className="donwpdfimg"
@@ -328,12 +137,13 @@ const Workwithus = () => {
                           </tr>
 
                           <tr>
-                            <td>Team Leader Position- Nursing </td>
-                            <td> --- </td>
+                            <td>Marketing</td>
+                            <td>Business Development Officer</td>
+                            <td>1-5 years</td>
                             <td>
                               <div className="applybtn">
                                 <span className="downpdg">
-                                  <Link href="/JobPDF/JD-TL Position Nursing-17-02-24.pdf" target="_blank" >
+                                  <Link href="/JobPDF/JD-Referral-Mark-22-6-24.pdf" target="_blank" >
                                     <Image width={100} height={100}
                                       src="/commonimages/download-pdf.png"
                                       className="donwpdfimg"
@@ -368,7 +178,7 @@ const Workwithus = () => {
             </div>
 
 
-          </div> */}
+          </div>
 
 
         </div>
